b47a177429bcd20892b242e29cbddb974dd301e3
TAABAKE4 <kevin.bates@swisscom.com>
2018-07-10 13:57:45 +0200

    [Major] whatever it is you want

--- Tests/PSMarkdown.tests.ps1
+++ Tests/PSMarkdown.tests.ps1
@@ -3,7 +3,7 @@ Import-Module -Force -Name .\AppVeyorCI-Tests.psd1
 Describe "Testing PSMarkdown" {
     Context "Importing the Module" {
         it "[PSMarkdown][Module] The module should be available after the import" {
-            $Module = Get-Module AppVeyorCI-Test
+            $Module = Get-Module AppVeyorCI-Tests
             $Module | should be $true
         }
     }
